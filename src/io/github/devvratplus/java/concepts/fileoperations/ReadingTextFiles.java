f92ba3d5a36778ee28b121d498511ef25a5b5387
Devvrat Shukla <devvratshukla@gmail.com>
2015-03-09 21:13:11 +0530

    Added ReadingTextFiles using Scanner class

---
+++ src/io/github/devvratplus/java/concepts/fileoperations/ReadingTextFiles.java
@@ -0,0 +1,25 @@
+package io.github.devvratplus.java.concepts.fileoperations;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadingTextFiles {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File textFile = new File(System.getProperty("user.dir") + "/files/readme.txt");
+		
+		Scanner in = new Scanner(textFile);
+		StringBuilder sb = new StringBuilder();
+		
+		while(in.hasNextLine()){
+			sb.append(in.nextLine() + "\n");			
+		}
+		
+		System.out.println(sb);
+				
+		in.close();
+	}
+
+}
